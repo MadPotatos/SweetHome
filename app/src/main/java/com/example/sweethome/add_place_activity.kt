9c4616d8b1ba5027eeff7b70a5cae049a7772e6a
VietNguyen <viet7896@gmail.com>
2022-10-03 23:40:59 +0700

    Initial commit

---
+++ app/src/main/java/com/example/sweethome/add_place_activity.kt
@@ -0,0 +1,11 @@
+package com.example.sweethome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class add_place_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_place)
+    }
+}
