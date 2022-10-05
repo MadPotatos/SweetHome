acf6dbd8d1f6666af3c249f7784297efb9f071b9
VietNguyen <viet7896@gmail.com>
2022-10-05 22:34:58 +0700

    Permission ,Dexter Third party

--- app/src/main/java/com/example/sweethome/AddPlaceActivity.kt
+++ app/src/main/java/com/example/sweethome/AddPlaceActivity.kt
@@ -1,13 +1,26 @@
 package com.example.sweethome
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.DatePickerDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
+import android.widget.Toast
 import com.example.sweethome.databinding.ActivityAddPlaceBinding
-import com.example.sweethome.databinding.ActivityMainBinding
+
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import java.text.SimpleDateFormat
 import java.util.*
+
 
 class AddPlaceActivity : AppCompatActivity(),View.OnClickListener {
     private var cal = Calendar.getInstance()
@@ -31,6 +44,7 @@ class AddPlaceActivity : AppCompatActivity(),View.OnClickListener {
             updateDateInView()
         }
         binding.etDate.setOnClickListener(this)
+        binding.tvAddImage.setOnClickListener(this)
     }
 
     override fun onClick(p0: View?) {
@@ -40,12 +54,63 @@ class AddPlaceActivity : AppCompatActivity(),View.OnClickListener {
                 ,cal.get(Calendar.MONTH),cal.get(Calendar.DAY_OF_MONTH)).show()
 
             }
-
+            binding.tvAddImage.id ->{
+                val pictureDialog = AlertDialog.Builder(this)
+                pictureDialog.setTitle("Select Action")
+                val pickerDialogItems = arrayOf("Select photo from Gallery","Take photo from camera")
+                pictureDialog.setItems(pickerDialogItems){
+                    _,which ->
+                    when(which){
+                        0 -> choosePhotoFromGallery()
+                        1 -> Toast.makeText(this@AddPlaceActivity,"Coming soon...",Toast.LENGTH_SHORT).show()
+                    }
+                }
+                pictureDialog.show()
+            }
         }
     }
+
+    private fun choosePhotoFromGallery() {
+        Dexter.withContext(this).withPermissions(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE).
+        withListener(object: MultiplePermissionsListener {
+            override fun onPermissionsChecked(report: MultiplePermissionsReport)
+            {if(report.areAllPermissionsGranted()){
+                Toast.makeText(this@AddPlaceActivity,"Permission Granted",Toast.LENGTH_SHORT).show()
+            }}
+            override fun onPermissionRationaleShouldBeShown(permissions: MutableList<PermissionRequest>, token: PermissionToken)
+            {
+                showRationalDialogForPermissions()
+            }
+        }).onSameThread().check()
+
+    }
+
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this).setMessage(
+            "You have turned off permission required for this feature" +
+                    "It can be enabled under the" + " Applications Settings"
+        ).setPositiveButton("GO TO SETTINGS") { _, _ ->
+            try {
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package", packageName, null)
+                intent.data = uri
+                startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                e.printStackTrace()
+            }
+        }.setNegativeButton("Cancel"){
+            dialog, which ->
+            dialog.dismiss()
+        }.show()
+    }
+
+
     private fun updateDateInView(){
         val format = "dd.MM.yyyy"
         val sdf = SimpleDateFormat(format,Locale.getDefault())
         binding.etDate.setText(sdf.format(cal.time).toString())
+
     }
 }
